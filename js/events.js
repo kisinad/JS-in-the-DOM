78f74869110f4f260d7235dab6b57fef28a3091d
Denis Kisina <kisinad@gmail.com>
2019-06-22 11:03:47 +0300

    added events

--- js/events.js
+++ js/events.js
@@ -1,3 +1,28 @@
+// const p = document.querySelector('p');
+// const button = document.querySelector('button');
+
+// const changeText = () => {
+//     p.textContent = "Will I change?";
+// }
+
+// const alertText = () => {
+//     alert('Will I alert?');
+// }
+
+// // Events can be overwritten
+// button.onclick = changeText;
+// button.onclick = alertText;
+// Function to modify the text content of the paragraph
+// const changeText = () => {
+//     const p = document.querySelector('p');
+
+//     p.textContent = "I changed because of an event listener.";
+// }
+
+// // Listen for click event
+// const button = document.querySelector('button');
+// button.addEventListener('click', changeText);
+
 const p = document.querySelector('p');
 const button = document.querySelector('button');
 
@@ -9,6 +34,6 @@ const alertText = () => {
     alert('Will I alert?');
 }
 
-// Events can be overwritten
-button.onclick = changeText;
-button.onclick = alertText;
+// Multiple listeners can be added to the same event and element
+button.addEventListener('click', changeText);
+button.addEventListener('click', alertText);
