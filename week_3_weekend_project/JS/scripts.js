68fbf122224ad64ab141b604461a7f85e989d1dc
Denis Kisina <kisinad@gmail.com>
2019-06-24 09:04:31 +0300

    edited scripts.js

--- week_3_weekend_project/JS/scripts.js
+++ week_3_weekend_project/JS/scripts.js
@@ -3,6 +3,8 @@
 var h1 = document.querySelector("h1");
 h1.style.color = "#900C3F";
 h1.style.textAlign="center";
+var x = 0;
+var array = Array();
 
 // Function stores variable on clicking save
 function contact_function() {
@@ -22,19 +24,32 @@ function contact_function() {
     	alert("The Second name must have no more than 10 characters");  
     	Submitted = "false";
 	}
-	if (fname.length=="" || sname.length=="") {
+	if (fname.length=="" || sname.length=="" || tel.length==""|| email.length==""||address=="") {
     	alert("Name field can not be empty!");  
     	Submitted = "false";
 	}
+	else
+	array[0] =fname;
+	array[1]=sname;
+	array[2]=email;
+	array[3]=address;
+
 	alert("Saved!");
+	//alert(array[0]);
 	
 
 }
 //Function to search through saved contacts
 function search_function(){
-	contactFields.style.display = "block";
-  var div = document.createElement("div");
-  div.innerHTML = fname.value;
-  contactFields.appendChild(div);
+
+ for (var y=0; y<array.length; y++){
+ 	var e=array[y];
+ 	
+ //  contactFields.appendChild(div);
+ }
+ // contactFields.style.display = "block";
+ //  var div = document.createElement("div");
+ //  div.innerHTML = fname.value;
+ document.getElementById("search").innerHTML = e;
 }
 
